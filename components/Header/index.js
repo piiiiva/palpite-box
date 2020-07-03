a6a3be96056ee3d39f18f5dff13085e662062e05
PivaDesk <piiiiva@gmail.com>
2020-07-03 01:03:02 -0300

    First commit

---
+++ components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className="container mx-auto">
+        Palpitebox
+      </div>
+    </div>
+  )
+}
+export default Header
