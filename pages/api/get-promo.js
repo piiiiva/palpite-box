da461a438a18a54ac68ef32b8a9c8d832a73ec4e
piiiiva <piiiiva@gmail.com>
2020-07-11 20:05:58 -0300

    Connecting with google-spreadsheet

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,6 +1,30 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import credencials from '../../credencials.json'
+
+const doc = new GoogleSpreadsheet('1bXi87Wwm-9bZLZ6z8L85FIOFMzreQYxFxG2XBK8W1GM')
+
 export default async (req, res) => {
-  res.end(JSON.stringify({
-    showCoupon: true,
-    message: 'Coupon message'
-  }))
+  try {
+    await doc.useServiceAccountAuth(credencials)
+    await doc.loadInfo()
+
+    const sheet = doc.sheetsByIndex[2] // selecionando terceira aba da planilha
+    await sheet.loadCells('A2:B2') // selecionando células especificas da planilha
+
+    const mostrarPromocaoCell = sheet.getCell(1, 0) // linha 1 coluna 0
+    const textoCell = sheet.getCell(1, 1) // linha 1, coluna 1
+
+    res.end(JSON.stringify({
+      showCoupon: mostrarPromocaoCell.value === 'VERDADEIRO',
+      message: textoCell.value
+    }))
+
+  } catch (err) {
+    res.end(JSON.stringify({
+      showCoupon: false,
+      message: ''
+    }))
+
+  }
+
 }
--- pages/api/save.js
+++ pages/api/save.js
@@ -1,4 +1,26 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import credencials from '../../credencials.json'
+
+const doc = new GoogleSpreadsheet('1bXi87Wwm-9bZLZ6z8L85FIOFMzreQYxFxG2XBK8W1GM')
+
 export default async (req, res) => {
-  console.log(JSON.parse(req.body))
-  res.end(req.body)
+  try {
+    await doc.useServiceAccountAuth(credencials)
+    await doc.loadInfo()
+    const sheet = doc.sheetsByIndex[1] // selecionando segunda aba da planilha
+    const data = JSON.parse(req.body)
+
+    await sheet.addRow({
+      Nome: data.Nome,
+      Email: data.Email,
+      Whatsapp: data.Whatsapp,
+      Cupom: 'aaa112',
+      Promo: 'Promo1'
+    })
+    res.end(req.body)
+
+  } catch (err) {
+    console.log(err)
+    res.end('error')
+  }
 }
