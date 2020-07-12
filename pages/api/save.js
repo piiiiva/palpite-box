09825d52dcb42c215d951d2f3be7b2cfded602b1
piiiiva <piiiiva@gmail.com>
2020-07-12 00:20:28 -0300

    Using useState in research form

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,7 +1,13 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 import credencials from '../../credencials.json'
+import moment from 'moment'
 
 const doc = new GoogleSpreadsheet('1bXi87Wwm-9bZLZ6z8L85FIOFMzreQYxFxG2XBK8W1GM')
+
+const genCupom = () => {
+  const code = parseInt(moment().format('YYMMDDHHmmSSSS')).toString(16).toUpperCase()
+  return code.substr(0, 4) + '-' + code.substr(4, 4) + '-' + code.substr(8, 4)
+}
 
 export default async (req, res) => {
   try {
@@ -10,12 +16,30 @@ export default async (req, res) => {
     const sheet = doc.sheetsByIndex[1] // selecionando segunda aba da planilha
     const data = JSON.parse(req.body)
 
+    const sheetConfig = doc.sheetsByIndex[2] // selecionando terceira aba da planilha
+    await sheetConfig.loadCells('A2:B2') // selecionando células especificas da planilha
+
+    const mostrarPromocaoCell = sheetConfig.getCell(1, 0) // linha 1 coluna 0
+    const textoCell = sheetConfig.getCell(1, 1) // linha 1, coluna 1
+
+    let Cupom = ''
+    let Promo = ''
+
+    if (mostrarPromocaoCell.value === 'VERDADEIRO') {
+      // TODO:  Gerar cupom
+      // Código gerado baseado na data em milessegundos, bem IMPROVÁVEL que dê problema
+      Cupom = genCupom()
+      Promo = textoCell.value
+    }
+
     await sheet.addRow({
       Nome: data.Nome,
       Email: data.Email,
       Whatsapp: data.Whatsapp,
-      Cupom: 'aaa112',
-      Promo: 'Promo1'
+      Cupom,
+      Promo,
+      'Data Preenchimento': moment().format('DD/MM/YYYY, HH:mm:ss'),
+      Nota: 5
     })
     res.end(req.body)
 
--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -1,16 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 
 const Pesquisa = () => {
+  const [form, setForm] = useState({
+    Nome: '',
+    Email: '',
+    Whatsapp: ''
+  })
+
   const save = async () => {
-    const form = {
-      Nome: 'aaa',
-      Email: 'bbb',
-      Whatsapp: 'ccc'
-    }
-
     try {
-
       const response = await fetch('/api/save', {
         method: 'POST',
         body: JSON.stringify(form) // transforma o form em uma string
@@ -19,8 +18,17 @@ const Pesquisa = () => {
       const data = await response.json()
       console.log(data)
     } catch (err) {
-
     }
+  }
+
+  const handleOnChange = evt => {
+    const targetValue = evt.target.value // precisa colocar em "cache" antes, não da pra atribuir direto no Nome
+    const key = evt.target.name
+
+    setForm(old => ({
+      ...old,
+      [key]: targetValue
+    }))
   }
 
   return (
@@ -29,11 +37,44 @@ const Pesquisa = () => {
       <p className='text-center mb-6'>
         O restaurante x sempre busca por atender melhorseus clientes. <br />
         Por isso, estamos sempre abertos a ouvir sua opinião.
-          </p>
+      </p>
       <div className='w-1/5 mx-auto'>
         <label className='font-bold'>Seu nome:</label>
-        <input type="text" className='p-4 block shadow bg-blue-100 my-2 rounded' />
-        <button className="bg-blue-400 px-6 py-4 font-bold rounded-lg shadow-lg hover:shadow" onClick={save}>Salvar</button>
+        <input
+          type="text"
+          className='p-4 block shadow bg-blue-100 my-2 rounded'
+          placeholder='Nome'
+          onChange={handleOnChange}
+          name='Nome'
+          value={form.Nome}
+        />
+
+        <label className='font-bold'>Seu e-mail:</label>
+        <input
+          type="text"
+          className='p-4 block shadow bg-blue-100 my-2 rounded'
+          placeholder='E-mail'
+          onChange={handleOnChange}
+          name='Email'
+          value={form.Email}
+        />
+
+        <label className='font-bold'>Seu whatsapp:</label>
+        <input
+          type="text"
+          className='p-4 block shadow bg-blue-100 my-2 rounded'
+          placeholder='Whatsapp'
+          onChange={handleOnChange}
+          name='Whatsapp'
+          value={form.Whatsapp}
+        />
+
+        <button
+          className="bg-blue-400 px-6 py-4 font-bold rounded-lg shadow-lg hover:shadow"
+          onClick={save}>Salvar</button>
+        <pre>
+          {JSON.stringify(form, null, 2)}
+        </pre>
       </div>
     </div>
   )
