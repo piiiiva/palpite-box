04c8db0b71aa27f4e71006361adef3f3cf780bad
piiiiva <piiiiva@gmail.com>
2020-07-12 21:31:38 -0300

    converting SHEE_PRIVATE_KEY to base64

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -2,9 +2,14 @@ import { GoogleSpreadsheet } from 'google-spreadsheet'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
+const fromBase64 = value => {
+  const buff = Buffer.from(value, 'base64')
+  return buff.toString('ascii')
+}
+
 export default async (req, res) => {
+  console.log(fromBase64(process.env.SHEET_PRIVATE_KEY))
   try {
-    // await doc.useServiceAccountAuth(credencials)
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
       private_key: process.env.SHEET_PRIVATE_KEY
